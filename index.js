7a1e5dcfe152e7d58a4bc9c1b7edea74913e3627
Matthew Pietz <sirlancelot@users.noreply.github.com>
2014-06-27 15:14:41 -0700

    Use only filename for remainingRequest
    Fix for when options.context is not cwd.

--- index.js
+++ index.js
@@ -5,15 +5,16 @@
 var path = require("path");
 module.exports = function() {};
 module.exports.pitch = function(remainingRequest) {
+	var basename = "." + path.sep + path.basename(remainingRequest);
 	this.cacheable && this.cacheable();
 	return "require(" + JSON.stringify("!" + path.join(__dirname, "addScript.js")) + ")"+
 			"(require(" +
-			JSON.stringify("!" + require.resolve("raw-loader") + "!" + remainingRequest) + ")" +
+			JSON.stringify("!" + require.resolve("raw-loader") + "!" + basename) + ")" +
 				(this.debug ?
 					"+" +
 						JSON.stringify(
 							"\n\n// SCRIPT-LOADER FOOTER\n//@ sourceURL=script:///" +
-								encodeURI(remainingRequest.replace(/^!/, "")).replace(/%5C|%2F/g, "/").replace(/\?/, "%3F").replace(/^\//, "")
+								encodeURI(basename.replace(/^!/, "")).replace(/%5C|%2F/g, "/").replace(/\?/, "%3F").replace(/^\//, "")
 						) :
 					"") +
 			")";
